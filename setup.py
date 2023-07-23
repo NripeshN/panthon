69df6891fc7b09fb56bf219b45fbea3008655f10
NripeshN <nn2012@hw.ac.uk>
2023-07-23 16:45:48 +0530

    fix lint

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
+"""
+Setup file for the Panthon package. Specifies package details and dependencies.
+"""
 from setuptools import setup, find_packages
 
-# Read in requirements.txt
-with open('requirements.txt') as f:
+# Read in requirements.txt with specified encoding
+with open('requirements.txt', 'r', encoding='utf-8') as f:
     requirements = f.read().splitlines()
 
 setup(
     name='panthon',
     version='0.1.0',
-    url='https://github.com/NripeshN/panthon',
-    author='Nripesh Niketan',
-    author_email='nripesh14@gmail.com',
+    url='https://github.com/yourusername/panthon',
+    author='Author Name',
+    author_email='author@gmail.com',
     description='A Machine Learning-powered Cybersecurity Attack Simulation Library',
     packages=find_packages(),
     install_requires=requirements,
@@ -22,5 +25,8 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
 )
