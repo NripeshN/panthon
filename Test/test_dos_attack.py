17ab6756b5b6e64ad7f50a872d004d6ef38d2bcb
NripeshN <nn2012@hw.ac.uk>
2023-07-23 21:17:12 +0530

    update test

--- Test/test_dos_attack.py
+++ Test/test_dos_attack.py
@@ -2,21 +2,28 @@ import unittest
 from src import dos_attack
 import threading
 import socket
+import time
 
 
 class DummyServer:
-    def __init__(self, host="localhost", port=9999):
+    def __init__(self, host="localhost", port=50000):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.bind((host, port))
         self.sock.listen(1)
+        self.sock.settimeout(1.0)
         self.connections = []
+        self.running = True
 
     def start(self):
-        while True:
-            conn, addr = self.sock.accept()
-            self.connections.append(conn)
+        while self.running:
+            try:
+                conn, addr = self.sock.accept()
+                self.connections.append(conn)
+            except socket.timeout:
+                continue
 
     def stop(self):
+        self.running = False  # stop the server
         for conn in self.connections:
             conn.close()
         self.sock.close()
@@ -36,11 +43,10 @@ class TestDoSAttack(unittest.TestCase):
         self.server_thread.join()
 
     def test_simulation(self):
-        dos = dos_attack.DoSAttack(
-            "localhost", 9999, 5
-        )  # target localhost, port 9999, 5 connections
+        dos = dos_attack.DoSAttack("localhost", 50000, 5)  
         initial_connections = self.server.connection_count()
         dos.simulate_attack()
+        time.sleep(2)
         dos.wait_for_threads()
         final_connections = self.server.connection_count()
         self.assertEqual(final_connections - initial_connections, 5)
