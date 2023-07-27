aec77a036bc31eaebb6bdc2f1db406e21e2b3d1a
NripeshN <nn2012@hw.ac.uk>
2023-07-27 23:45:25 +0530

    update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt', 'r', encoding='utf-8') as f:
 
 setup(
     name='panthon',
-    version='0.1.1',
+    version='0.1.2',
     url='https://github.com/nripeshn/panthon',
     author='Nripesh Niketan',
     author_email='nripesh14@gmail.com',
