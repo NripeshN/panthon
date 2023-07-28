f6b161a0808e9389fa58bf75d76fd3195202a29a
NripeshN <nn2012@hw.ac.uk>
2023-07-28 22:56:13 +0530

    update init.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 from .dos_attack import DoSAttack
 from .ddos_attack import DDoSAttack
-from .sql_injection import SQLInjection
+from .sql_injection import SQLInjectionAttack
 from .mitm_attack import MITMAttack
 from .dns_spoofer import DNSSpoofer
 from .xss_attack import XSSAttack
