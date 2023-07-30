b4f2c0ef7aee8be63995b1697537a2aa7ec4e780
NripeshN <nn2012@hw.ac.uk>
2023-07-30 11:21:56 +0530

    dos update

--- src/dos_attack.py
+++ src/dos_attack.py
@@ -70,11 +70,3 @@ class DoSAttack:
     def wait_for_threads(self):
         for thread in self.threads:
             thread.join()
-
-
-if __name__ == "__main__":
-    dos = DoSAttack(
-        "192.168.1.1", 80, 500, "Slowloris"
-    )  # target IP, target port, number of connections, type of attack
-    dos.simulate_attack()
-    dos.wait_for_threads()
