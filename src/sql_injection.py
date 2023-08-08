908548570959b232c873c5c405397c0c50398abb
NripeshN <nn2012@hw.ac.uk>
2023-08-08 11:34:54 +0530

    sql injection change template

--- src/sql_injection.py
+++ src/sql_injection.py
@@ -7,42 +7,30 @@ import os
 
 
 class SQLInjectionAttack:
-    def __init__(self, target_url, level=5, risk=3, attack_type="sqlmap"):
-        self.target_url = target_url
-        self.attack_type = attack_type
-        self.level = level
-        self.risk = risk
-
-    def simulate_attack(self):
-        if self.attack_type == "sqlmap":
-            self.sqlmap_attack()
-        elif self.attack_type == "TimeBasedBlind":
-            self.time_based_blind_attack()
-        elif self.attack_type == "ErrorBased":
-            self.error_based_attack()
-        elif self.attack_type == "UnionBased":
-            self.union_based_attack()
-        else:
-            raise ValueError("Invalid attack type!")
-
-    def sqlmap_attack(self):
-        logging.info("Attacking %s with SQLMap...", self.target_url)
+    def sqlmap_attack(self, target_url, level=5, risk=3):
+        logging.info("Attacking %s with SQLMap...", target_url)
         path_to_executable = os.path.join(os.path.dirname(__file__), "sqlmap/sqlmap.py")
         command = [
             "python3",
             path_to_executable,
             "-u",
-            self.target_url,
+            target_url,
             "--batch",
             "--level",
-            str(self.level),
+            str(level),
             "--risk",
-            str(self.risk),
+            str(risk),
         ]
         subprocess.run(command)
 
 
-sql_injection_attack = SQLInjectionAttack(
-    "http://localhost:3000/rest/products/search?q=", attack_type="sqlmap"
-)  # target_url, attack_type
-sql_injection_attack.simulate_attack()
+# Create SQLInjectionAttack object
+sql_injection_attack = SQLInjectionAttack()
+
+# SQLMap attack parameters
+target_url_sqlmap = "http://localhost:3000/rest/products/search?q="
+level = 5
+risk = 3
+
+# Launch SQLMap attack
+sql_injection_attack.sqlmap_attack(target_url_sqlmap, level, risk)
