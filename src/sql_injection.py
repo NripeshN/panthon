8169565b3fde913f3a76d0f1288d90584ecfef2f
NripeshN <nn2012@hw.ac.uk>
2023-08-10 11:59:23 +0530

    update example usage

--- src/sql_injection.py
+++ src/sql_injection.py
@@ -77,4 +77,4 @@ class SQLInjectionAttack:
 sql_injection_attack = SQLInjectionAttack()
 target_url = "http://localhost:3000/rest/products/search?q="
 sql_injection_attack.sqli_scanner(target_url)
-sql_injection_attack.sqlmap_attack(target_url, database="test")
+sql_injection_attack.sqlmap_attack(target_url)
