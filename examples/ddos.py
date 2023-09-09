6bcc0e5be4be72e7ea8296f276f039275c14070c
NripeshN <nripesh14@gmail.com>
2023-09-09 22:27:44 +0400

    import fix

--- examples/ddos.py
+++ examples/ddos.py
@@ -1,7 +1,2 @@
-from src.ddos_attack import BotNet  # change this to import from panthon
+import panthon
 
-botnet = BotNet(
-    10, "192.168.1.1", 80, 100
-)  # num of bots, target IP, target port, connections per bot
-botnet.create_bots()
-botnet.launch_attack()
