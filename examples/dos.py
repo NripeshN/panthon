900b47b6660d4a1f21d96ab4299b03b06cadf004
NripeshN <nripesh14@gmail.com>
2023-09-10 18:58:19 +0400

    import fix example dos

--- examples/dos.py
+++ examples/dos.py
@@ -1,4 +1,4 @@
-from panthon.src.dos_attack import DoSAttack
+from panthon import DoSAttack
 
 dos = DoSAttack()  # target_url, num_connections, attack_type
 dos.slowloris_attack(url="https://panthon.app", target_port=80, num_connections=100)
