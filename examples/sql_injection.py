a1682791dc842173dc986c00b8e1ad222b37ad90
Nripesh Niketan <86844847+NripeshN@users.noreply.github.com>
2023-09-19 21:13:01 +0400

    Update sql_injection.py

--- examples/sql_injection.py
+++ examples/sql_injection.py
@@ -1,6 +1,5 @@
 from panthon import SQLInjectionAttack
 
-# Example usage:
 sql_injection_attack = SQLInjectionAttack()
 target_url = "http://localhost:3000/rest/products/search?q="
 sql_injection_attack.sqli_scanner(target_url)
