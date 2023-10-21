59e64f0e50b30bffee398cafa764d123f2673d12
NripeshN <nripesh14@gmail.com>
2023-10-21 23:06:11 +0400

    remove unnecessary comments

--- examples/dos.py
+++ examples/dos.py
@@ -1,5 +1,5 @@
 from panthon import DoSAttack
 
-dos = DoSAttack()  # target_url, num_connections, attack_type
+dos = DoSAttack()
 dos.slowloris_attack(url="https://panthon.app", target_port=80, num_connections=100)
 dos.goldeneye_attack(url="https://panthon.app")
